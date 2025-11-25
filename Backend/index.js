74d430d6e10818aac3e823cbd8554271a01995b6
NourNafady <125382864+NourNafady@users.noreply.github.com>
2025-11-25 16:44:26 +0200

    Add Middleware BodyParser Without Handling SignUp & Login yet

--- Backend/index.js
+++ Backend/index.js
@@ -1,16 +1,26 @@
 import express from "express";
+import bodyParser from "body-parser";
 
 const port = 3000;
 const app = express();
 
-// Get API Check 
-app.get('/', (req, res) => {
-    res.send("Hello World From Server");
-})
+app.use(bodyParser.urlencoded({extended:true}));
 
+// Get API Check 
+app.get("/", (req, res) => {
+    // console.log(req.body);
+    res.send("Hello World From Server");
+});
+
+// For testing and Understand Something Not for the App
+// To Be Deleted !!!!!!!!
+app.post("/register", (req, res) => {
+    console.log(req.body);
+    res.sendStatus(200);
+})
 
 // Listening 
 app.listen(port,"localhost", (err) => {
     if (err) throw err;
     console.log(`Server Listening on Port ${port}`);
-})
+});
